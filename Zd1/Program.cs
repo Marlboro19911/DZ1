3bfba5dbc472627bd6dba0abce81d4fa68ee5073
Yakov <Marlboro19911@gmail.com>
2023-01-23 22:27:08 +0300

    Первая домашняя работа

---
+++ Zd1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт,
+ какое число большее, а какое меньшее.
+Пример: a = 5; b = 7 ->  max = 7, min = 5
+a = -9 b = -3 -> max = -3, min = -9 */
+ 
+Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine("a>b");
+}
+else if (b>a)
+{
+    Console.WriteLine("a<b");
+}
+else
+{
+    Console.WriteLine("a=b") ;
+}
